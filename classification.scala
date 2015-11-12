740e5be621acd49b705f7f8e9c0a5ccd0e6e70ed
onsDridi <ons.dridi@cetic.be>
2015-11-12 18:21:08 +0100

    Update classification.scala

--- classification.scala
+++ classification.scala
@@ -62,3 +62,64 @@ val nbModel = NaiveBayes.train(nbData)
 ############Créer le modèle Arbre de decision##############
 val dtModel = DecisionTree.train(data, Algo.Classification, Entropy, maxTreeDepth)
 
+############Générer les prédictions##############
+//Générer des prédiction pour un seul individus des données 
+val dataPoint = data.first
+val prediction = lrModel.predict(dataPoint.features)
+val trueLabel = dataPoint.label
+
+
+//Générer des prédictions pour tout le RDD Data qu'on a crée
+val predictions = lrModel.predict(data.map(lp => lp.features))
+predictions.take(5)
+
+
+###########Evaluer la performance du modèle crée##############
+//Dans la formule suivante, on va tester l'indicateur "Prediction accuracy and error" pour le modèle Regression logistique
+val lrTotalCorrect = data.map { point =>
+  if (lrModel.predict(point.features) == point.label) 1 else 0
+}.sum
+val lrAccuracy = lrTotalCorrect / numData
+
+
+//Taux d'exactitude pour les autres modèles
+val svmTotalCorrect = data.map { point =>
+  if (svmModel.predict(point.features) == point.label) 1 else 0
+}.sum
+val nbTotalCorrect = nbData.map { point =>
+  if (nbModel.predict(point.features) == point.label) 1 else 0
+}.sum
+val dtTotalCorrect = data.map { point =>
+  val score = dtModel.predict(point.features)
+  val predicted = if (score > 0.5) 1 else 0 
+  if (predicted == point.label) 1 else 0
+}.sum
+val svmAccuracy = svmTotalCorrect / numData
+val nbAccuracy = nbTotalCorrect / numData
+val dtAccuracy = dtTotalCorrect / numData
+
+
+############Améliorer la performance des modèles##############
+//Standardization des variables
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+val vectors = data.map(lp => lp.features)
+val matrix = new RowMatrix(vectors)
+val matrixSummary = matrix.computeColumnSummaryStatistics()
+println(matrixSummary.mean)
+println(matrixSummary.min)
+println(matrixSummary.variance)
+
+//Use the StandardScaler for the standardization
+import org.apache.spark.mllib.feature.StandardScaler
+val scaler = new StandardScaler(withMean = true, withStd = true).fit(vectors)
+val scaledData = data.map(lp => LabeledPoint(lp.label, scaler.transform(lp.features)))
+
+// Construire le modèle de regression logistique sur les données normalisées
+val lrModelScaled = LogisticRegressionWithSGD.train(scaledData, numIterations)
+val lrTotalCorrectScaled = scaledData.map { point =>
+  if (lrModelScaled.predict(point.features) == point.label) 1 else 0
+}.sum
+val lrAccuracyScaled = lrTotalCorrectScaled / numData
+
+
+
